2f14312edefb692069d197962722cf1047f147f6
Charles Calvert <java@elvenware.com>
2013-11-20 11:59:11 -0800

    Bare bones angular, jasmine, karma projet

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 AngularKarma
 ============
+
+This is a bare bones project that provides support for:
+
+- Angular
+- Jasmine
+- Karma 
+
---
+++ Source/Main.js
@@ -0,0 +1,14 @@
+ /**
+ * @author Charlie Calvert
+ */
+
+ /* jshint devel: true */
+
+angular.module('mainModule', [])
+.controller('mainController', function($scope) { 'use strict';
+
+	$scope.name = "mainController";
+});
+
+
+
